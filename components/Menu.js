0aebb31ecbfc30e75f742874a5c991f8fe48936e
Daniel Schmitz <danieljfa@gmail.com>
2020-11-10 00:12:13 -0300

    teste de menu

--- components/Menu.js
+++ components/Menu.js
@@ -1,12 +1,12 @@
 import { List, ListItem, ListItemIcon, ListItemText } from "@material-ui/core";
-import MailIcon from '@material-ui/icons/Mail';
 import AccountBalanceIcon from '@material-ui/icons/AccountBalance';
+import CategoryIcon from '@material-ui/icons/Category';
 
 export default function Menu() {
     return <List>
-            <ListItem button key='1'>
-              <ListItemIcon><MailIcon /></ListItemIcon>
-              <ListItemText primary="Item 1"/>
+            <ListItem button key='1' component="a" href="/categories">
+              <ListItemIcon><CategoryIcon /></ListItemIcon>
+              <ListItemText primary="Categorias"/>
             </ListItem>
             <ListItem button key='2'>
               <ListItemIcon><AccountBalanceIcon /></ListItemIcon>
