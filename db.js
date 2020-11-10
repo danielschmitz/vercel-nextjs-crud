249a2d249751697d027778dc88e431b6490c31c6
Daniel Schmitz <danieljfa@gmail.com>
2020-11-09 21:17:38 -0300

    mudança no db para ativar auto import do vscode

--- db.js
+++ db.js
@@ -3,4 +3,6 @@ require('dotenv').config()
 const env = process.env.NODE_ENV || "development"
 
 const knexfile = require('./knexfile')
-module.exports = require('knex')(env)
+const db = require('knex')(env)
+
+module.exports = db
